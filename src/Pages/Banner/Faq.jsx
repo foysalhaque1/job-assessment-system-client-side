4bec111d7a7d6f62dc21566887f19e322922be7d
user <foysalhaque.mist.ipe@gmail.com>
2025-08-10 11:39:36 +0600

    why we built this platform section is added

--- src/Pages/Banner/Faq.jsx
+++ src/Pages/Banner/Faq.jsx
@@ -1,27 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const Faq = () => {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        fetch('../../../public/faq.json').then(res => res.json())
-            .then(data => {
-                setData(data)
-            })
-    });
-    console.log(data)
+    
+    
+    
     return (
-        <div className='w-11/12 mx-auto bg-amber-100 p-3'>
-            <h2 className='text-2xl font-bold text-center'>FAQ Section</h2>
-            {
-                data.map(dat => 
-                    <div className='max-w-6xl mx-auto border m-8 p-7 rounded-2xl'>
-                        <h1>Question: {dat.q1}</h1>
-                        <h2>Answer: {dat.a1}</h2>
+         <div className='w-11/12 mx-auto bg-[#dedcff] p-5'>
+            <h2 className='text-2xl font-bold text-center'>Why We Built This Platform</h2>
+             <div className='max-w-6xl mx-auto border m-8 p-7 rounded-2xl'>
+                        <h1 className='text-blue-600 font-bold'>Make Collaboration Easy   </h1>
+                        <h2 className='font-bold text-black'>All your assignments, submissions, and feedback are in one place. You won’t have to juggle between apps or search through endless messages anymore. </h2>
 
                     </div>
-               )
+             <div className='max-w-6xl mx-auto border m-8 p-7 rounded-2xl'>
+                        <h1 className='text-blue-600 font-bold'>Encourage Peer Learning   </h1>
+                        <h2 className='font-bold text-black'>Every user is both a learner and a teacher. You can review your friends’ work, learn from their methods, and share your own ideas.</h2>
 
-            }
+                    </div>
+             <div className='max-w-6xl mx-auto border m-8 p-7 rounded-2xl'>
+                        <h1 className='text-blue-600 font-bold'>Stay Organized  </h1>
+                        <h2 className='font-bold text-black'>Deadlines, grading, and submissions are clearly tracked. You will always know what’s pending and what’s done.</h2>
+
+                    </div>
+             <div className='max-w-6xl mx-auto border m-8 p-7 rounded-2xl'>
+                        <h1 className='text-blue-600 font-bold'>Accessible Anytime, Anywhere   </h1>
+                        <h2 className='font-bold text-black'>Whether you’re at home, in a café, or on the train, you can create, complete, and grade assignments from any device. </h2>
+
+                    </div>
+             <div className='max-w-6xl mx-auto border m-8 p-7 rounded-2xl'>
+                        <h1 className='text-blue-600 font-bold'>Motivate Through Feedback   </h1>
+                        <h2 className='font-bold text-black'>Constructive comments and marks from friends help you improve. This keeps the learning process encouraging and fun.</h2>
+
+                    </div>
         </div>
     );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Faq2 from './Banner/Faq2';
+import Faq from './Banner/Faq';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-           <Faq2></Faq2>
+            <Faq2></Faq2>
+            <div className='mt-4'>
+
+                <Faq></Faq>
+            </div>
         </div>
     );
 };
